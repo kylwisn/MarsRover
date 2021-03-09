a5f0b1fd5c5730b65c6a73ddad4507c4482a66c2
Kyle <72363241+kylwisn@users.noreply.github.com>
2021-03-08 23:22:56 -0500

    Add files via upload

---
+++ MarsRover/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+
+// Mission Objective: 
+// Rover: Position and location represented by (x,y,z). Where z is a compass direction (N, E, W, S)
+//  2 rovers in play
+// Plateau: Rectulangualr grid positions (x,y,z). Where (0,0,N) means X=0, Y=0, Z=N or bottom left of the grid and the rover is facing north. (0,1) is up one grid block from (0,0) and (1,1) is one grid block to the right of (0,1).
+//      Maximum Coordinates -> (Max x, Max y)
+// Command (Message): String, (aaaaaaaa) represented by (L, R, M) where L= spin left 90 degrees left, R= spins rover 90 degrees right and M= moves the rover forward by 1.
+// Input = 5 lines
+//  1. Plateau size (5:5)
+//  2. Array of RoverInstruction objects, where RoverInstruction object contains:
+//      a. RoverInstruction -> (first line)
+//      b. RoverCommand -> (Second Line)
+// Output = 2 lines
+//
+// Expected behaviors
+//  1. Rover should spin left: SpinLeft()
+//  2. Rover should spin right: SpinRight()
+//  3. Rover should move forward: StepForward()
+//  4. Rover should go to 1 N N after the first set of inputs
+//  5. Rover should now move. 
+
+
+
+namespace MarsRover
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //string startLocation;
+            string moveCommand;
+            string startLocation;
+
+            //Enter starting location
+            Console.Write("First Rover: Enter starting location: ");
+            startLocation = Console.ReadLine();
+
+            //Enter the instruction string
+            Console.Write("First Rover: Enter instruction string: ");
+            moveCommand = Console.ReadLine();
+
+            //Create rover1 object
+            Rover rover1 = new Rover(startLocation);
+            
+            //Launching the Move function of rover1 with the moveCommand string
+            rover1.Move(moveCommand);
+
+            //Reporting the final position of rover1
+            Console.WriteLine("Rover 1 final position: " + rover1.newLocation);
+
+
+            //Visual line break for separating rovers in the console
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            //Enter starting location
+            Console.Write("Second Rover: Enter starting location: ");
+            startLocation = Console.ReadLine();
+
+            //Enter the instruction string
+            Console.Write("Second Rover: Enter instruction string: ");
+            moveCommand = Console.ReadLine();
+
+            //Create rover2 object
+            Rover rover2 = new Rover(startLocation);
+
+            //Launching the Move function of rover2 with the moveCommand string
+            rover2.Move(moveCommand);
+
+            //Reporting the final position of rover2
+            Console.WriteLine("Rover 2 final position: " + rover2.newLocation);
+
+            // End Main
+        }
+
+
+    }
+}
---
+++ MarsRover/Rover.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarsRover
+{
+
+    public class Rover
+    {
+        public int x; // x coordinate of the current rover's position
+        public int y; // y cooridnate of the current rover's position
+        public string direction; // cardinal (compass) direction of the current rover position
+        public string newLocation; // Outputting the updated location
+
+
+        public Rover(string location) // location = "1 2 N"
+        {
+            Int32.TryParse(location.Split(" ")[0], out x);
+            Int32.TryParse(location.Split(" ")[1], out y);
+            direction = location.Split(" ")[2];
+
+            // End Rover directions input
+        }
+
+        public void SpinLeft() //Rover spins left
+        {
+            switch (direction)
+            {
+                case "N":
+                    direction = "W";
+                    break;
+                case "E":
+                    direction = "N";
+                    break;
+                case "S":
+                    direction = "E";
+                    break;
+                case "W":
+                    direction = "S";
+                    break;
+                default:
+                    throw new ArgumentException();
+            }
+
+            // End SpinLeft
+        }
+
+        public void SpinRight() //Rover spins right
+        {
+            switch (direction)
+            {
+                case "N":
+                    direction = "E";
+                    break;
+                case "E":
+                    direction = "S";
+                    break;
+                case "S":
+                    direction = "W";
+                    break;
+                case "W":
+                    direction = "N";
+                    break;
+                default:
+                    throw new ArgumentException();
+            }
+
+            // End SpinRight
+        }
+
+        public void StepForward() //Rover steps forward
+        {
+            switch (direction)
+            {
+                case "N":
+                    y++; //add one
+                    break;
+                case "E":
+                    x++; //add one
+                    break;
+                case "S":
+                    y--; //subtract one
+                    break;
+                case "W":
+                    x--; //subtract one
+                    break;
+                default:
+                    throw new ArgumentException();
+            }
+
+            // End StepForward
+        }
+
+
+        public void Move(string roverCommand) // Moving the rover with the roverCommand string input. Example: "LMLMLMLMM" or "MMRMMRMRRM"
+        {
+            //Char Array
+            char[] instructions = roverCommand.ToCharArray();
+
+            // loop through array. For each letter, call SpinLeft, SpinRight, or StepForward as commanded.
+            foreach (char i in instructions)
+            {
+
+                switch (i)
+                {
+                    case 'L':
+                        SpinLeft();
+                        break;
+                    case 'R':
+                        SpinRight();
+                        break;
+                    case 'M':
+                        StepForward();
+                        break;
+                    default:
+                        throw new ArgumentException();
+                }
+
+                // end for loop
+            }
+            
+            //defining newLocation as the expected output coordinates
+            newLocation = x + " " + y + " " + direction;
+
+            // End Move
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
